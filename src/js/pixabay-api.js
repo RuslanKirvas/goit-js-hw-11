b9aea4dff3a6f427e0dd598ef27fbf38c8a5f7bc
RuslanKirvas <kirvas.ruslan@gmail.com>
2026-01-20 19:51:36 +0100

    hw-11-js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
 const BASE_URL = 'https://pixabay.com/api/';
-const API_KEY = import.meta.env.VITE_PIXABAY_KEY;
+// const API_KEY = import.meta.env.VITE_PIXABAY_KEY;
+// VITE_PIXABAY_KEY=54244805-3431f894e484dfd9b88b609d0
 
+const API_KEY = '54244805-3431f894e484dfd9b88b609d0';
 if (!API_KEY) {
   console.error('❌ VITE_PIXABAY_KEY не найден в .env');
 }
